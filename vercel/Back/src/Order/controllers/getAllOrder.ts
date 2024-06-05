05cd0dc931b9513689981258eebd474dce9ee648
CEQuinterogarcia <kamterogarcia11@gmail.com>
2024-06-05 02:03:57 -0500

    ruta sin aut

--- vercel/Back/src/Order/controllers/getAllOrder.ts
+++ vercel/Back/src/Order/controllers/getAllOrder.ts
@@ -1,14 +1,12 @@
 import { Request, Response } from 'express';
-//import { Order } from '../../models/Order';
+import { Order } from '../../models/Order';
 
 const getAllOrder = async (_req: Request, res: Response): Promise<void> => {
   try {
-    // const orders = await Order.findAll({
-    //    include: [{ all: true, required: false  }],
-    // });
-    const user = process.env.EMAIL_ELECTROEMPORIUM;
-   // pass: process.env.PASSWORD_ELECTROEMPORIUM
-    res.json(user);
+    const orders = await Order.findAll({
+       include: [{ all: true, required: false  }],
+    });
+    res.json(orders);
   } catch (error) {
     console.error('Error fetching orders:', error);
     res.status(500).json({ error: 'Internal server error' });
